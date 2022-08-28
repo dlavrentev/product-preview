a85223f683e0bdb41717abc3a68b7290336f4da3
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-28 11:06:36 +0200

    Update Component.jsx

--- src/Components/Component.jsx
+++ src/Components/Component.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./../Components/Component.scss";
+import ImgMobile from './../images/image-product-mobile.jpg'
 
 const Component = () => {
-  return <div>Hellooo</div>;
+  return <div className="container">
+    <div className="container_image">
+      <img src={ImgMobile} alt="parfume" />
+    </div>
+    <div className="container_info">
+      
+    </div>
+  </div>;
 };
 
 export default Component;
