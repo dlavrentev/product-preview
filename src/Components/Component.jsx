1f7ab07624506b454e75e370848bca9b5734502e
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-28 11:11:50 +0200

    Update Component.jsx

--- src/Components/Component.jsx
+++ src/Components/Component.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import "./../Components/Component.scss";
-import ImgMobile from './../images/image-product-mobile.jpg'
+import ImgMobile from "./../images/image-product-mobile.jpg";
+import Cart from "./../images/icon-cart.svg";
 
 const Component = () => {
-  return <div className="container">
-    <div className="container_image">
-      <img src={ImgMobile} alt="parfume" />
+  return (
+    <div className="container">
+      <div className="container_image">
+        <img src={ImgMobile} alt="parfume" />
+      </div>
+      <div className="container_info">
+        <p>PERFUME</p>
+        <h1>Gabrielle Essence Eau De Parfum</h1>
+        <p>
+          A floral, solar and voluptuous interpretation composed by Olivier
+          Polge, Perfumer-Creator for the House of CHANEL.
+        </p>
+        <div className="price">
+          <h1>$149.99</h1>
+          <p>$169.99</p>
+        </div>
+        <button>
+          <img src={Cart} alt="cart" /> Add to Cart
+        </button>
+      </div>
     </div>
-    <div className="container_info">
-      
-    </div>
-  </div>;
+  );
 };
 
 export default Component;
