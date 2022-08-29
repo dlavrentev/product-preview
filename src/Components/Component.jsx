2e378a6de2ebfa5846f2f2ca5d16f408e9f7c11b
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-29 09:54:48 +0200

    updated css

--- src/Components/Component.jsx
+++ src/Components/Component.jsx
@@ -8,15 +8,15 @@ const Component = () => {
     <div className="container">
       <img src={ImgMobile} alt="parfume" />
       <div className="container_info">
-        <p>PERFUME</p>
-        <h1>Gabrielle Essence Eau De Parfum</h1>
-        <p>
+        <p className="category">PERFUME</p>
+        <h1 className="product">Gabrielle Essence Eau De Parfum</h1>
+        <p className="desc">
           A floral, solar and voluptuous interpretation composed by Olivier
           Polge, Perfumer-Creator for the House of CHANEL.
         </p>
         <div className="price">
-          <h1>$149.99</h1>
-          <p>$169.99</p>
+          <h1 className="newprice">$149.99</h1>
+          <p className="oldprice">$169.99</p>
         </div>
         <button>
           <img src={Cart} alt="cart" /> Add to Cart
